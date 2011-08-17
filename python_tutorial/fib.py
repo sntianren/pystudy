eb108b2a21c5d71ec6397f5d920d6014f4329a81
Stephen Lao <sntianren@gmail.com>
2011-08-17 11:19:50 +0800

    study python tutorial

---
+++ python_tutorial/fib.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# Filename: fib.py
+
+def fib(n):     # write Fibonacci series up to n
+    """Print a Fibonacci series up to n."""
+    a, b = 0, 1
+    while a < n:
+        print(a, end = ' ')
+        a, b = b, a + b
+    print()
